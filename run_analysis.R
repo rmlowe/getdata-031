6528dde716078071af3ecfd149b6be34af8a75ac
Robert Lowe <robertmlowe@rmlowe.com>
2015-08-16 13:11:14 +0800

    Getting and Cleaning Data Course Project: Reading the training data

--- run_analysis.R
+++ run_analysis.R
@@ -11,6 +11,24 @@ if (!file.exists(destfile)) {
   download.file(url = dataUrl, destfile = destfile)
 }
 
+# Step 1
+# Merge the training and the test sets to create one data set.
+train <- read.table(file = unz(description = destfile,
+                               filename = "UCI HAR Dataset/train/X_train.txt"))
+
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement. 
+
+# Step 3
+# Use descriptive activity names to name the activities in the data set
+
+# Step 4
+# Appropriately label the data set with descriptive variable names. 
+
+# Step 5
+# From the data set in step 4, create a second, independent tidy data set with the average of each
+# variable for each activity and each subject.
+
 # Create txt file
-x <- data.frame(foo = c("X", "Y"), bar = c(1, 2))
-write.table(x = x, file = "./data/tidy.txt", row.names = FALSE)
+tidy <- data.frame(foo = c("X", "Y"), bar = c(1, 2))
+write.table(x = tidy, file = "./data/tidy.txt", row.names = FALSE)
